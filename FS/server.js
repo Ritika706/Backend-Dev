c8df839c9ad4e54769010c9b38488cd2ef213c96
Ritika Soni <soniritika172006@gmail.com>
2026-01-09 15:06:44 +0530

    updated files

--- FS/server.js
+++ FS/server.js
@@ -46,6 +46,42 @@ const fs = require("fs");
 // })
 
 // delete function
-fs.unlinkSync('./output.txt')
+// fs.unlinkSync('./output.txt')
 
-console.log("end");
+// console.log("end");
+
+//path module
+const path =require("path");
+//absolute path
+// const absolutePath =path.resolve("./log.txt")
+// console.log(absolutePath)
+// console.log(__dirname)
+// console.log(path.basename('./notes/log.txt'))//file ka name ayega
+// console.log(path.extname('./notes/log.txt'))//file ka extension dikhayega
+// const joinPath=path.join(__dirname,"notes","log.txt");
+// console.log(joinPath)
+// const pathParse=path.parse(joinPath)
+// console.log(pathParse);
+const filePath=path.join(__dirname,"log","log.txt")
+const data =fs.readFileSync(filePath,"utf-8");
+console.log(data)
+//http method
+//get--read
+//post--create
+//put--update
+//delete--delete
+//patch--partial update
+//api=application programming interface=do server k beech m communicate krne k liye
+//rest api is a type of api which use http to communicate b/w server and backend
+const http=require("http")
+const server=http.createServer((req,res)=>{
+    console.log(req.url)
+    console.log(req.method)
+    res.writeHead(200,{"content-type":"text/html"})
+    res.write("<h1>bhelcome</h1>")
+    res.end("hello from asus")
+})
+
+server.listen(3000,()=>{
+    console.log("server is running on port",3000)
+})
